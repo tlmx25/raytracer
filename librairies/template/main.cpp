fca33938c0b9ffaa51d36ac761a8dc3adeba42cb
Tom <tom.lefoix@epitech.eu>
2024-04-22 16:45:40 +0200

    add: structure of project

---
+++ librairies/template/main.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2024
+** arcade
+** File description:
+** main
+*/
+
+
+#include <iostream>
+#include <memory>
+
+
+void test_shared()
+{
+    std::cout << "Test shared" << std::endl;
+}
