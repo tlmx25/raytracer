d901e0be90a7a2a721169b053244369cf1d35f6b
root <arthur.doriel@epitech.eu>
2024-04-30 15:54:38 +0200

    feat(main): switch occurs)

--- src/camera/camera.hpp
+++ src/camera/camera.hpp
@@ -19,6 +19,14 @@ class camera {
     int samples_per_pixel = 10;
     int max_depth = 10;
 
+    double vfov = 90;
+    point3 lookfrom = point3(0,0,0);
+    point3 lookat   = point3(0,0,-1);
+    vec3   vup      = vec3(0,1,0);
+
+    double defocus_angle = 0;
+    double focus_dist = 10; 
+
     void render(const hittable& world) {
         initialize();
 
@@ -46,6 +54,9 @@ class camera {
     point3 pixel00_loc;
     vec3 pixel_delta_u;
     vec3 pixel_delta_v;
+    vec3 u, v, w;
+    vec3   defocus_disk_u;
+    vec3   defocus_disk_v;
 
     void initialize() {
         image_height = int(image_width / aspect_ratio);
@@ -53,18 +64,29 @@ class camera {
 
         pixel_samples_scale = 1.0 / samples_per_pixel;
 
-        center = point3(0, 0, 0);
+        center = lookfrom;
 
-        auto focal_length = 1.0;
-        auto viewport_height = 2.0;
+        auto theta = degrees_to_radians(vfov);
+        auto h = tan(theta/2);
+        auto viewport_height = 2 * h * focus_dist;
         auto viewport_width = viewport_height * (double(image_width)/image_height);
-        auto viewport_u = vec3(viewport_width, 0, 0);
-        auto viewport_v = vec3(0, -viewport_height, 0);
+
+        w = unit_vector(lookfrom - lookat);
+        u = unit_vector(cross(vup, w));
+        v = cross(w, u);
+
+        vec3 viewport_u = viewport_width * u;
+        vec3 viewport_v = viewport_height * -v; 
+
         pixel_delta_u = viewport_u / image_width;
         pixel_delta_v = viewport_v / image_height;
-        auto viewport_upper_left =
-            center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
+
+        auto viewport_upper_left = center - (focus_dist * w) - viewport_u/2 - viewport_v/2;
         pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+        auto defocus_radius = focus_dist * tan(degrees_to_radians(defocus_angle / 2));
+        defocus_disk_u = u * defocus_radius;
+        defocus_disk_v = v * defocus_radius;
     }
 
     ray get_ray(int i, int j) const {
@@ -73,10 +95,15 @@ class camera {
         auto pixel_sample = pixel00_loc
                           + ((i + offset.x()) * pixel_delta_u)
                           + ((j + offset.y()) * pixel_delta_v);
-        auto ray_origin = center;
+        auto ray_origin = (defocus_angle <= 0) ? center : defocus_disk_sample();
         auto ray_direction = pixel_sample - ray_origin;
 
         return ray(ray_origin, ray_direction);
+    }
+
+    point3 defocus_disk_sample() const {
+        auto p = random_in_unit_disk();
+        return center + (p[0] * defocus_disk_u) + (p[1] * defocus_disk_v);
     }
 
     vec3 sample_square() const {
