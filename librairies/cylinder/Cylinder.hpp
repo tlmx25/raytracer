e7b100d9af35bd03e781357db6a6a784ee09647c
MaxenceLgt <maxence.largeot@epitech.eu>
2024-05-06 09:06:15 +0200

    refactor(feature/cylinder): Cylinder class functions - problem to solve during display

--- librairies/cylinder/Cylinder.hpp
+++ librairies/cylinder/Cylinder.hpp
@@ -11,9 +11,23 @@
 
 class Cylinder : public APrimitive {
     public:
-        Cylinder();
+        Cylinder(double radius, const Vec3& center, shared_ptr<IMaterial> mat);
+        Cylinder(const libconfig::Setting &settings);
+        Cylinder(const Cylinder &obj);
         ~Cylinder();
+    public:
+        bool hit(const Ray& r, Interval ray_t, HitRecord& rec) const override;
+    public:
+        Cylinder &operator=(const Cylinder &obj);
     private:
+        static void get_cylinder_uv(const Vec3 &p, double &u, double &v) {
+            double theta = std::acos(-p.y());
+            double phi = atan2(-p.z(), p.x()) + M_PI;
+            u = phi / (2 * M_PI);
+            v = theta / M_PI;
+        };
+        double _radius;
+        Vec3 _center;
 };
 
 extern "C" IPrimitive *entryPoint(const libconfig::Setting &settings);
